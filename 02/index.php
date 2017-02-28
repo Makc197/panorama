fa1a2211f86372e8826d3f34cf6c2b89fa045dcd
MAKC <maxim.s.lazarev@gmail.com>
2017-02-28 14:39:45 +0300

    first commit

---
+++ 02/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		body {
+			margin: 0;
+		}	 
+
+		#bg {
+			animation: bg 60s linear infinite;
+			backface-visibility: hidden;
+			transform: translate3d(0,0,0);
+			background-color: #348cb2;
+			background-image: url("img.jpg");
+			background-position: bottom left;
+			background-repeat: repeat-x;
+			height: 100%;
+			left: 0;
+			opacity: 1;
+			position: absolute;
+			top: 0;
+			background-size: contain;
+			width: 6000px;
+		}
+		@keyframes bg {
+
+			0% {
+				transform: translate3d(0,0,0); 
+			}
+
+			100% { 
+				transform: translate3d(-10000px,0,0); 
+			} 
+		}
+
+	</style>
+</head>
+<body>
+	<div id="bg"></div>
+	<div></div>
+</body>
+</html>
